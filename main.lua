69cde610e6fda5cca53653d7cd3ce7badab1b190
Xun Ruan <galaxymacos@gmail.com>
2020-06-20 21:35:43 -0400

    Initial commit

---
+++ main.lua
@@ -0,0 +1,39 @@
+function love.load()
+  sprites = {}
+  sprites.player = love.graphics.newImage('sprites/player.png')
+  sprites.bullet = love.graphics.newImage('sprites/bullet.png')
+  sprites.zombie = love.graphics.newImage('sprites/zombie.png')
+  sprites.background = love.graphics.newImage('sprites/background.png')
+
+  player = {}
+  player.x = 200
+  player.y = 200
+  player.speed = 100
+
+end
+
+function love.update(dt)
+  if love.keyboard.isDown("s") then
+
+    player.y = player.y + player.speed * dt;
+  end
+
+  if love.keyboard.isDown("w") then
+    player.y = player.y - player.speed * dt;
+  end
+
+  if love.keyboard.isDown("a") then
+    player.x = player.x - player.speed * dt;
+  end
+
+  if love.keyboard.isDown("d") then
+    player.x = player.x + player.speed * dt;
+  end
+
+
+end
+
+function love.draw()
+  love.graphics.draw(sprites.background, 0, 0)
+  love.graphics.draw(sprites.player, player.x, player.y, -math.pi/2)
+end
